9f94ac70a29b3b6ae36c91204978ab97f40b9087
Pavlo Hupalo <pashkogup@gmail.com>
2021-02-09 09:58:40 +0200

    Add destroy method

--- plugins/modal.js
+++ plugins/modal.js
@@ -28,9 +28,13 @@ $.modal = function(options) {
     const ANIMATION_SPEED = 300;
     const $modal = _createModal(options);
     let closing = false;
+    let destroyed = false;
 
     const modal = {
         open() {
+            if(destroyed) {
+                return console.log('Modal is destroyed');
+            }
             !closing && $modal.classList.add('open');
         },
         close() {
@@ -41,16 +45,21 @@ $.modal = function(options) {
                 $modal.classList.remove('close');
                 closing = false;
             }, ANIMATION_SPEED)
-        },
-        destroy() {}
-
+        }
     }
-
-    $modal.addEventListener('click', event => {
-        console.log('Clicked', event.target.dataset.close)
+    const listener = event => {
         if(event.target.dataset.close) {
             modal.close();
         }
+    }
+
+    $modal.addEventListener('click', event => listener);
+
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal);
+            $modal.removeEventListener('click', listener);
+            destroyed = true;
+        }
     })
-    return modal
 }
